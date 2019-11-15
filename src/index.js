706d381096d3054dec603a8fa5b2d62aab5ac205
Marcell Endrey <marcell.endrey@prezi.com>
2019-11-15 15:38:27 +0100

    fix: import functions to be used

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 
+const {handleGithubEvent} = require('./github')
+const {handleSlackEvent} = require('./slack')
+
 express()
     .use(express.json())
     .post('/event/github', handleGithubEvent)
