7076f1d4e4427d2e9113e575cf2575b7d2b349d0
Marcell Endrey <marcell.endrey@prezi.com>
2019-12-28 11:17:26 +0100

    chore: setup jest with babel for testing

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    verbose: true,
+    testMatch: ['<rootDir>/test/**/*.mjs'],
+    transform: {
+        '^.+\\.mjs$': 'babel-jest',
+    },
+    moduleFileExtensions: ['js', 'mjs'],
+}
