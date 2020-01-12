1d8df8534cd8347e797160f8a99c2ac61d52fc9e
Marcell Endrey <marcell.endrey@prezi.com>
2020-01-12 11:00:04 +0100

    test: merging pr deletes it from the storage

--- test/app.test.mjs
+++ test/app.test.mjs
@@ -9,6 +9,14 @@ const MOCK_PR_URL = 'https://github.com/test-user/test-repo/pull/1'
 const MOCK_CHANNEL = 'mock-channel'
 const MOCK_TIMESTAMP = 'mock-timestamp'
 
+const MOCK_STORAGE_CONTENTS = [
+    {
+        pr_url: MOCK_PR_URL,
+        message_channel: MOCK_CHANNEL,
+        message_timestamp: MOCK_TIMESTAMP,
+    },
+]
+
 describe('Smoke', () => {
     test('receive slack message', async () => {
         const mockAddReaction = jest.fn(() => Promise.resolve())
@@ -44,5 +52,47 @@ describe('End-to-end', () => {
             action: 'approved',
         })
         expect(mockAddReaction).toBeCalledWith('white_check_mark', MOCK_CHANNEL, MOCK_TIMESTAMP)
+    })
+
+    describe('Actions affecting the storage', () => {
+        test('Commenting has no effect', async () => {
+            const mockAddReaction = jest.fn(() => Promise.resolve())
+            const app = new PrmojiApp(new TestStorage(MOCK_STORAGE_CONTENTS), new TestClient(mockAddReaction))
+            await app.handlePrEvent({
+                url: MOCK_PR_URL,
+                action: 'commented',
+            })
+            expect((await app.storage.get(MOCK_PR_URL)).rows.length).toBe(1)
+        })
+
+        test('Requesting changes has no effect', async () => {
+            const mockAddReaction = jest.fn(() => Promise.resolve())
+            const app = new PrmojiApp(new TestStorage(MOCK_STORAGE_CONTENTS), new TestClient(mockAddReaction))
+            await app.handlePrEvent({
+                url: MOCK_PR_URL,
+                action: 'changes_requested',
+            })
+            expect((await app.storage.get(MOCK_PR_URL)).rows.length).toBe(1)
+        })
+
+        test('Approving has no effect', async () => {
+            const mockAddReaction = jest.fn(() => Promise.resolve())
+            const app = new PrmojiApp(new TestStorage(MOCK_STORAGE_CONTENTS), new TestClient(mockAddReaction))
+            await app.handlePrEvent({
+                url: MOCK_PR_URL,
+                action: 'approved',
+            })
+            expect((await app.storage.get(MOCK_PR_URL)).rows.length).toBe(1)
+        })
+
+        test('Merging removes entry from the storage', async () => {
+            const mockAddReaction = jest.fn(() => Promise.resolve())
+            const app = new PrmojiApp(new TestStorage(MOCK_STORAGE_CONTENTS), new TestClient(mockAddReaction))
+            await app.handlePrEvent({
+                url: MOCK_PR_URL,
+                action: 'merged',
+            })
+            expect((await app.storage.get(MOCK_PR_URL)).rows.length).toBe(0)
+        })
     })
 })
