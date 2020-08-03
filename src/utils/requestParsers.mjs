5a59fde24838eadbb067b92c0f714750d2bdcc57
Marcell Endrey <marcell.endrey@prezi.com>
2020-08-03 10:24:27 +0200

    Revert "chore: killme debug logging, where are my labels?"
    This reverts commit 2028208a5534dfbef0250c200d045cad118e51d2.

--- src/utils/requestParsers.mjs
+++ src/utils/requestParsers.mjs
@@ -11,7 +11,6 @@ import {
 } from './helpers.mjs'
 
 export function parseGithubRequest(request) {
-    console.log(JSON.stringify(request.body, null, 2))
     return {
         url: getPrUrl(request.body),
         action: getPrAction(request),
